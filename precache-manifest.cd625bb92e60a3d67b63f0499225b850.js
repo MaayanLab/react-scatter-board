3a53608f5ea17955b3150b865c33d66152ab2c30
wangz10 <wangzc921@gmail.com>
2019-08-30 16:53:06 -0400

    Updates

---
+++ precache-manifest.cd625bb92e60a3d67b63f0499225b850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dbf1178e70e0a628675",
+    "url": "/react-scatter-board/static/js/runtime~main.6dbf1178.js"
+  },
+  {
+    "revision": "8319b1f8671d596b80e3",
+    "url": "/react-scatter-board/static/js/main.8319b1f8.chunk.js"
+  },
+  {
+    "revision": "993350bf3f985c582e6d",
+    "url": "/react-scatter-board/static/js/1.993350bf.chunk.js"
+  },
+  {
+    "revision": "8319b1f8671d596b80e3",
+    "url": "/react-scatter-board/static/css/main.f62ff25a.chunk.css"
+  },
+  {
+    "revision": "8f4746bdb2321faec019e5cbbcdda988",
+    "url": "/react-scatter-board/index.html"
+  }
+];
