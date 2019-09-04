401d985ccf07974dc1bd31ac3e0b47a6950b1912
wangz10 <wangzc921@gmail.com>
2019-09-04 11:12:48 -0400

    Code split for util functions

--- src/scatterPlot.js
+++ src/scatterPlot.js
@@ -5,136 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 import _ from 'underscore'
 import Texture from './texture'
-/**
- * convenience for converting JSON color to rgba that canvas wants
- * Be nice to handle different forms (e.g. no alpha, CSS style, etc.)
- */
-
-function getCanvasColor(color) {
-  return 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + color.a + ')'
-}
-const RARE = 'other'
-
-function getDataType(value) {
-  let type = typeof value
-  if (type === 'number') {
-    type = 'int'
-    if (!Number.isInteger(value)) {
-      type = 'float'
-    }
-  }
-  return type
-}
-
-function orderArray(arr, indices) {
-  // Reorder arr based on indices
-  const orderedArr = new Array(arr.length)
-  for (let i = 0; i < arr.length; i++) {
-    orderedArr[i] = arr[indices[i]]
-  }
-  return orderedArr
-}
-
-function encodeRareCategories(arr, k) {
-  // Count occurences of each unique categories in arr,
-  // then keep top k and encode rare categories as 'rares'
-  let counts = _.countBy(arr)
-  // sort values
-  counts = _.sortBy(_.pairs(counts), tuple => -tuple[1])
-  // get top k frequent categories
-  const frequentCategories = _.map(counts.slice(0, k), tuple => tuple[0])
-  for (let i = 0; i < arr.length; i++) {
-    if (frequentCategories.indexOf(arr[i]) === -1) {
-      arr[i] = RARE
-    }
-  }
-  return arr
-}
-
-function binValues(arr, nbins) {
-  // Binning continues array of values in to nbins
-  var extent = d3.extent(arr)
-  var min = parseFloat(extent[0])
-  var max = parseFloat(extent[1])
-  var interval = (max - min) / nbins // bin width
-
-  var domain = _.range(1, nbins).map(function(i) {
-    return i * interval + min
-  }) // bin edges
-  var labels = [min.toFixed(2) + ' to ' + domain[0].toFixed(2)]
-
-  for (var i = 0; i < nbins - 1; i++) {
-    if (i === nbins - 2) {
-      // the last bin
-      var label = domain[i].toFixed(2) + ' to ' + max.toFixed(2)
-    } else {
-      var label = domain[i].toFixed(2) + ' to ' + domain[i + 1].toFixed(2)
-    }
-    labels.push(label)
-  }
-  return {
-    labels: labels,
-    domain: domain,
-    min: min,
-    max: max,
-    interval: interval
-  }
-}
-
-function binValues2(arr, domain) {
-  // Binning continues array of values by a given binEdges (domain)
-  // domain: [0.001, 0.01, 0.05, 0.1, 1]
-  // domain should include the largest (rightest) value
-  var extent = d3.extent(arr)
-  var min = parseFloat(extent[0])
-  var max = parseFloat(extent[1])
-
-  var labels = ['0 to ' + domain[0]]
-  var nbins = domain.length
-
-  for (var i = 0; i < nbins - 1; i++) {
-    var label = domain[i] + ' to ' + domain[i + 1]
-    labels.push(label)
-  }
-  return { labels: labels, domain: domain.slice(0, -1), min: min, max: max }
-}
-
-function binBy(list, key, nbins) {
-  // similar to _.groupBy but applying to continues values using `binValues`
-  // list: an array of objects
-  // key: name of the continues variable
-  // nbins: number of bins
-  var values = _.pluck(list, key)
-  var binnedValues = binValues(values, nbins)
-  var labels = binnedValues.labels
-  var min = binnedValues.min
-  var interval = binnedValues.interval
-
-  var grouped = _.groupBy(list, function(obj) {
-    var i = Math.floor((obj[key] - min) / interval)
-    if (i === nbins) {
-      // the max value
-      i = nbins - 1
-    }
-    return labels[i]
-  })
-  return grouped
-}
-
-function binBy2(list, key, domain) {
-  // wrapper for `binValuesBy`
-  var values = _.pluck(list, key)
-  var binnedValues = binValues2(values, domain)
-  var labels = binnedValues.labels
-
-  var grouped = _.groupBy(list, function(obj) {
-    var i = _.filter(domain, function(edge) {
-      return edge < obj[key]
-    }).length
-    return labels[i]
-  })
-  return grouped
-}
+import * as utils from './utils'
 
 class ScatterDataSubset {
   // "model" for PointsGeometry
@@ -210,7 +81,7 @@ class ScatterData {
       if (xyz.indexOf(key) === -1) {
         const values = data.map(o => o[key])
         const nUnique = [...new Set(values)].length
-        const type = getDataType(data[0][key])
+        const type = utils.getDataType(data[0][key])
         this.metas.push({
           name: key,
           nUnique: nUnique,
@@ -267,7 +138,7 @@ class ScatterData {
   }
 
   binBy(metaKey, nbins) {
-    const dataSubsets = binBy(this.data, metaKey, nbins)
+    const dataSubsets = utils.binBy(this.data, metaKey, nbins)
     const scatterDataSubsets = _.mapObject(dataSubsets, (records, key) => {
       return new ScatterDataSubset(records)
     })
@@ -275,7 +146,7 @@ class ScatterData {
   }
 
   binBy2(metaKey, domain) {
-    const dataSubsets = binBy2(this.data, metaKey, domain)
+    const dataSubsets = utils.binBy2(this.data, metaKey, domain)
     const scatterDataSubsets = _.mapObject(dataSubsets, (records, key) => {
       return new ScatterDataSubset(records)
     })
@@ -296,7 +167,7 @@ class ScatterData {
     this.metas.push({
       name: key,
       nUnique: [...new Set(values)].length,
-      type: getDataType(values[0])
+      type: utils.getDataType(values[0])
     })
   }
 
@@ -329,7 +200,7 @@ class ScatterData {
     const dtype = meta.type
 
     if (dtype !== 'float' && dtype !== 'int' && meta.nUnique > 20) {
-      metas = encodeRareCategories(metas, 19)
+      metas = utils.encodeRareCategories(metas, 19)
     }
     let uniqueCats = new Set(metas)
     const nUniqueCats = uniqueCats.size
@@ -348,26 +219,26 @@ class ScatterData {
       }
     }
     // Make RARE to be grey
-    if (uniqueCats.indexOf(RARE) !== -1) {
+    if (uniqueCats.indexOf(utils.RARE) !== -1) {
       if (uniqueCats.length === 20) {
-        const idx = uniqueCats.indexOf(RARE)
+        const idx = uniqueCats.indexOf(utils.RARE)
         let greyIdx2 = 15
         if (uniqueCats.indexOf('unknown') !== -1) {
           greyIdx2 = 14
         }
         const elem = uniqueCats[greyIdx2]
-        uniqueCats[greyIdx2] = RARE
+        uniqueCats[greyIdx2] = utils.RARE
         uniqueCats[idx] = elem
       }
     }
 
     // make colorScale
     let colorExtent = d3.extent(metas)
-    const min_score = colorExtent[0]
-    const max_score = colorExtent[1]
+    const minScore = colorExtent[0]
+    const maxScore = colorExtent[1]
     let colorScale = d3.scale
       .pow()
-      .domain([min_score, (min_score + max_score) / 2, max_score])
+      .domain([minScore, (minScore + maxScore) / 2, maxScore])
       .range(['#1f77b4', '#ddd', '#d62728'])
 
     if (meta.name === 'Scores' || meta.name === 'R2_Score') {
@@ -514,7 +385,7 @@ class ScatterData {
   }
 
   calculateShapeScale(metaKey) {
-    const symbols = d3.svg.symbolTypes.map(t => d3.svg.symbol().type(t)())
+    let symbols = d3.svg.symbolTypes.map(t => d3.svg.symbol().type(t)())
 
     // make shapeScale for d3.legend
     const meta = _.findWhere(this.metas, { name: metaKey })
@@ -532,19 +403,19 @@ class ScatterData {
       // get grouped datasets, each group is going to be a cloud
       scatterDataSubsets = this.binBy(metaKey, 6)
       // Make a threshold scale
-      const binnedValues = binValues(_.pluck(this.data, metaKey), 6)
+      let binnedValues = utils.binValues(_.pluck(this.data, metaKey), 6)
 
       if (meta.name === 'p-value') {
         // get grouped datasets, each group is going to be a cloud
         const pValueDomain = [0.001, 0.01, 0.05, 0.1, 1]
         scatterDataSubsets = this.binBy2(metaKey, pValueDomain)
         // Make a threshold scale
-        const binnedValues = binValues2(
+        binnedValues = utils.binValues2(
           _.pluck(this.data, metaKey),
           pValueDomain
         )
         // overwrite the symbols map to make it having the same length with pValueDomain
-        const symbols = _.map(
+        symbols = _.map(
           d3.svg.symbolTypes.slice(0, pValueDomain.length),
           function(t) {
             return d3.svg.symbol().type(t)()
@@ -635,7 +506,6 @@ class Scatter3dView extends React.Component {
         this.makeMaterial()
         this.setUpStage()
       } else if (this.props.data !== null && this.props.shapeScale !== null) {
-        // this.shapeBy(this.state.shapeKey) // will update state shapeScale etc.
         this.shapeBy(this.props.shapeKey) // will update state shapeScale etc.
         this.renderScatter()
         if (this.state.is3d) {
@@ -706,8 +576,6 @@ class Scatter3dView extends React.Component {
     // Put the renderer's DOM into the container
     renderer.domElement.id = 'renderer'
     this.mount.appendChild(renderer.domElement)
-    // this.mount.style.position = 'relative'
-    // this.mount.style.padding = 0
 
     // set up orbit controls
     const controls = new OrbitControls(camera, renderer.domElement)
@@ -748,10 +616,8 @@ class Scatter3dView extends React.Component {
     this.controls = controls
     this.raycaster = raycaster
     this.mouse = mouse
-
+    // set the id for this object
     this.id = scene.uuid
-
-    //   this.addMouseEvents();
   }
 
   handleMouseMove(e) {
@@ -805,11 +671,7 @@ class Scatter3dView extends React.Component {
   shapeBy(metaKey) {
     const model = this.props.model
     const scene = this.scene
-    const {
-      shapeScale,
-      shapeLabels,
-      scatterDataSubsets
-    } = model.calculateShapeScale(metaKey)
+    const { scatterDataSubsets } = model.calculateShapeScale(metaKey)
 
     // Update geometry by merging individual genmetries from each group
     const geometries = []
@@ -826,7 +688,7 @@ class Scatter3dView extends React.Component {
       return arr1.concat(arr2)
     })
     let labels = model.getLabels(this.state.labelKeys)
-    labels = orderArray(labels, indices)
+    labels = utils.orderArray(labels, indices)
 
     mergedGeometry.userData = { index: indices, labels: labels }
     const groupSizes = geometries.map(g => g.index.length)
@@ -847,16 +709,8 @@ class Scatter3dView extends React.Component {
     scene.add(points)
     this.points = points
 
-    // calculate colorScale
-    // const colorScale = model.calculateColorScale(this.props.colorKey)
+    // set colors
     this.setColors(this.props.colorScale, this.props.colorKey)
-    // this.setState({
-    //   colorScale: this.props.colorScale,
-    //   shapeKey: metaKey,
-    //   shapeScale: shapeScale,
-    //   shapeLabels: shapeLabels
-    // })
-    // this.resetColors()
   }
 
   renderScatter() {
@@ -944,7 +798,7 @@ class Scatter3dView extends React.Component {
     context.textAlign = 'left'
 
     // text color.  Note that we have to do this AFTER the round-rect as it also uses the "fillstyle" of the canvas
-    context.fillStyle = getCanvasColor(textColor)
+    context.fillStyle = utils.getCanvasColor(textColor)
 
     // calculate the project of 3d point into 2d plain
     const point = new THREE.Vector3(x, y, z).applyEuler(euler)
@@ -969,23 +823,21 @@ class Scatter3dView extends React.Component {
   }
 
   resetColors() {
-    // this.setColors(this.state.colorScale, this.state.colorKey)
     this.setColors(this.props.colorScale, this.props.colorKey)
   }
 
   setColors(colorScale, metaKey) {
     // Color points by a certain metaKey given colorScale
-    // const model = this.state.model
     const model = this.props.model
     let metas = model.getAttr(metaKey)
     const geometry = this.points.geometry
     const idx = geometry.userData.index
-    metas = orderArray(metas, idx)
+    metas = utils.orderArray(metas, idx)
 
     const meta = _.findWhere(model.metas, { name: metaKey })
     const dtype = meta.type
     if (dtype !== 'float' && dtype !== 'int' && meta.nUnique > 20) {
-      metas = encodeRareCategories(metas, 19)
+      metas = utils.encodeRareCategories(metas, 19)
     }
 
     // construct colors BufferAttribute
@@ -1005,7 +857,7 @@ class Scatter3dView extends React.Component {
       for (let i = metas.length - 1; i >= 0; i--) {
         let color = colorScale(metas[i])
         if (frequentCategories.indexOf(metas[i]) === -1) {
-          color = colorScale(RARE)
+          color = colorScale(utils.RARE)
         }
         color = new THREE.Color(color)
         color.toArray(colors, i * 3)
---
+++ src/utils.js
@@ -0,0 +1,143 @@
+import _ from 'underscore'
+import d3 from 'd3'
+/*
+ * Utility functions
+ */
+
+function getCanvasColor(color) {
+  return 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + color.a + ')'
+}
+const RARE = 'other'
+
+function getDataType(value) {
+  let type = typeof value
+  if (type === 'number') {
+    type = 'int'
+    if (!Number.isInteger(value)) {
+      type = 'float'
+    }
+  }
+  return type
+}
+
+function orderArray(arr, indices) {
+  // Reorder arr based on indices
+  const orderedArr = new Array(arr.length)
+  for (let i = 0; i < arr.length; i++) {
+    orderedArr[i] = arr[indices[i]]
+  }
+  return orderedArr
+}
+
+function encodeRareCategories(arr, k) {
+  // Count occurences of each unique categories in arr,
+  // then keep top k and encode rare categories as 'rares'
+  let counts = _.countBy(arr)
+  // sort values
+  counts = _.sortBy(_.pairs(counts), tuple => -tuple[1])
+  // get top k frequent categories
+  const frequentCategories = _.map(counts.slice(0, k), tuple => tuple[0])
+  for (let i = 0; i < arr.length; i++) {
+    if (frequentCategories.indexOf(arr[i]) === -1) {
+      arr[i] = RARE
+    }
+  }
+  return arr
+}
+
+function binValues(arr, nbins) {
+  // Binning continues array of values in to nbins
+  var extent = d3.extent(arr)
+  var min = parseFloat(extent[0])
+  var max = parseFloat(extent[1])
+  var interval = (max - min) / nbins // bin width
+
+  var domain = _.range(1, nbins).map(function(i) {
+    return i * interval + min
+  }) // bin edges
+  var labels = [min.toFixed(2) + ' to ' + domain[0].toFixed(2)]
+
+  for (var i = 0; i < nbins - 1; i++) {
+    if (i === nbins - 2) {
+      // the last bin
+      var label = domain[i].toFixed(2) + ' to ' + max.toFixed(2)
+    } else {
+      var label = domain[i].toFixed(2) + ' to ' + domain[i + 1].toFixed(2)
+    }
+    labels.push(label)
+  }
+  return {
+    labels: labels,
+    domain: domain,
+    min: min,
+    max: max,
+    interval: interval
+  }
+}
+
+function binValues2(arr, domain) {
+  // Binning continues array of values by a given binEdges (domain)
+  // domain: [0.001, 0.01, 0.05, 0.1, 1]
+  // domain should include the largest (rightest) value
+  var extent = d3.extent(arr)
+  var min = parseFloat(extent[0])
+  var max = parseFloat(extent[1])
+
+  var labels = ['0 to ' + domain[0]]
+  var nbins = domain.length
+
+  for (var i = 0; i < nbins - 1; i++) {
+    var label = domain[i] + ' to ' + domain[i + 1]
+    labels.push(label)
+  }
+  return { labels: labels, domain: domain.slice(0, -1), min: min, max: max }
+}
+
+function binBy(list, key, nbins) {
+  // similar to _.groupBy but applying to continues values using `binValues`
+  // list: an array of objects
+  // key: name of the continues variable
+  // nbins: number of bins
+  var values = _.pluck(list, key)
+  var binnedValues = binValues(values, nbins)
+  var labels = binnedValues.labels
+  var min = binnedValues.min
+  var interval = binnedValues.interval
+
+  var grouped = _.groupBy(list, function(obj) {
+    var i = Math.floor((obj[key] - min) / interval)
+    if (i === nbins) {
+      // the max value
+      i = nbins - 1
+    }
+    return labels[i]
+  })
+  return grouped
+}
+
+function binBy2(list, key, domain) {
+  // wrapper for `binValuesBy`
+  var values = _.pluck(list, key)
+  var binnedValues = binValues2(values, domain)
+  var labels = binnedValues.labels
+
+  var grouped = _.groupBy(list, function(obj) {
+    var i = _.filter(domain, function(edge) {
+      return edge < obj[key]
+    }).length
+    return labels[i]
+  })
+  return grouped
+}
+
+export {
+  RARE,
+  getCanvasColor,
+  getDataType,
+  orderArray,
+  encodeRareCategories,
+  binValues,
+  binValues2,
+  binBy,
+  binBy2
+}
