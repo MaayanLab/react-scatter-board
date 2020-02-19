d63d90a878e6947678a5c8c26d4adbb84633842e
Daniel J. B. Clarke <u8sand@gmail.com>
2020-02-19 10:12:49 -0500

    Added setup.py for installing as python dependency

---
+++ setup.py
@@ -0,0 +1,21 @@
+import json
+from setuptools import setup, find_packages
+
+packageJson = json.load(open('package.json', 'r'))
+
+setup(
+  name='react_scatter_board',
+  version=packageJson['version'],
+  description=packageJson['description'],
+  url=packageJson['homepage'],
+  author=packageJson['author'],
+  license=packageJson['license'],
+  long_description=open('README.md', 'r').read(),
+  packages=find_packages(),
+  include_package_data=True,
+  package_data={'': [
+    'react_scatter_board/package-info.json',
+    'react_scatter_board/react_scatter_board.min.js',
+  ]},
+  requires=['dash'],
+)
